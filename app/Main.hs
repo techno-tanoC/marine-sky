d809e17ce4ba2e30c08c48eae9ced872116df7ba
techno-tanoC <develop.tanoc@gmail.com>
2018-08-13 00:22:48 +0900

    implement a tracking app

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
+import MarineSky.Container as Container
+
+import Control.Concurrent
+import Control.Concurrent.MVar
+import Control.Monad.IO.Class
+import Data.Aeson (FromJSON)
+import GHC.Generics
+import Web.Scotty
+
+data Push = Push {
+  name :: String,
+  url :: String,
+  ext :: String
+} deriving (Show, Generic)
+
+instance FromJSON Push
+
+data Cancel = Cancel {
+  id :: String
+} deriving (Show, Generic)
+
+instance FromJSON Cancel
+
 main :: IO ()
-main = return ()
+main = do
+  ts <- Container.new
+
+  scotty 8080 $ do
+    get "/" $ do
+      vals <- liftIO $ Container.collect ts
+      liftIO $ print "get"
+      json vals
+
+    post "/" $ do
+      var <- liftIO $ newMVar True
+      liftIO $ print "post"
+      liftIO . forkIO $ Container.start ts var $ do
+        threadDelay $ 5 * 1000 * 1000
+        swapMVar var False
+        threadDelay $ 5 * 1000 * 1000
+      return ()
