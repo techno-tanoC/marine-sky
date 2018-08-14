43fa633cac35e32249e0a6dfa9e6fa0e824ccf22
techno-tanoC <develop.tanoc@gmail.com>
2018-08-14 17:31:13 +0900

    update MarineSky

--- src/MarineSky.hs
+++ src/MarineSky.hs
@@ -1,19 +1,24 @@
 module MarineSky (
   Env.new,
-  collect,
+  extract,
   start,
   cancel
 ) where
 
 import qualified MarineSky.Container as Container
 import MarineSky.Env as Env
+import MarineSky.HTTP as HTTP
+import MarineSky.Progress as Progress
+import MarineSky.Renamer (Name, Ext)
 import qualified MarineSky.Tracker as Tracker
 
-collect :: Env a -> IO [a]
-collect = Container.collect . Env.container
+import Network.HTTP.Client (Request, parseRequest_)
+
+extract :: Env a -> IO [Tracker.Tracker a]
+extract = Container.extract . Env.container
 
 cancel :: Env a -> String -> IO ()
 cancel = Container.cancel . Env.container
 
-start :: Env a -> String -> IO ()
-start env req = undefined
+start :: Env Progress -> String -> (Name, Ext) -> IO ()
+start env req n = HTTP.download env (parseRequest_ req) n
